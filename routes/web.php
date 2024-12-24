b8547d1ec636c7c3144a22bce019926d5aaf0224
GusDPA <gporcelarias@frba.utn.edu.ar>
2024-12-24 13:02:49 -0300

    merge

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
-use App\Http\Controllers\ProveedorController;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProveedorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,7 +25,40 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::get('/logout', [LoginController::class, 'logout']);
 
 Route::resource("usuario", UsuarioController::class)->middleware("auth");
-Route::resource("proveedor", ProveedorController::class)->middleware("auth");
+Route::resource("usuario", UsuarioController::class)->middleware("auth")->names([
+    'index' => 'usuarios.index',
+]);
+
+Route::get('/productos', [ProductoController::class, 'index']);
+Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
+
+
+Route::get('/categorias', [CategoriaController::class, 'index']);
+Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
+Route::resource("categorias", CategoriaController::class)->middleware("auth")->names([
+    'index' => 'categorias.index',
+    'create' => 'categorias.create',
+    'store' => 'categorias.store',
+    'show' => 'categorias.show',
+    'edit' => 'categorias.edit',
+    'update' => 'categorias.update',
+    'destroy' => 'categorias.destroy',
+]);
+
+Route::resource("proveedores", ProveedorController::class)->middleware("auth");
+
+Route::resource("usuario", UsuarioController::class)->middleware("auth")->names([
+    'index' => 'usuarios.index',
+    'create' => 'usuarios.create',
+    'store' => 'usuarios.store',
+    'show' => 'usuarios.show',
+    'edit' => 'usuarios.edit',
+    'update' => 'usuarios.update',
+    'destroy' => 'usuarios.destroy',
+]);
+
+Route::get('/usuario/{usuario}/password_edit', [UsuarioController::class, 'password_edit']);
+Route::post('/usuario/{usuario}/password_edit', [UsuarioController::class, 'password_update']);
 
 Route::get('/plantilla', function () {
     return view('plantilla');
