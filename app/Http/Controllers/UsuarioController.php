061d7fc8426b0372edc98d69e944baa97c85af59
GusDPA <gporcelarias@frba.utn.edu.ar>
2024-12-24 13:05:14 -0300

    merge

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\VarDumper\VarDumper;
 
-class   UsuarioController extends Controller
+class UsuarioController extends Controller
 {
     /**
      * Display a listing of the resource.
