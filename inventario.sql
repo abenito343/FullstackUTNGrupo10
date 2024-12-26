ad5447924b905b9cdd3b5cf4e9c49d12650c3b39
Ariel <abenito@frba.utn.edu.ar>
2024-12-26 13:01:02 -0300

    correciones base de datos

--- inventario.sql
+++ inventario.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-12-2024 a las 05:50:32
+-- Tiempo de generación: 26-12-2024 a las 17:00:25
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,98 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `inventario`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(55) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Cocina y Hogar', 'Para la casa'),
+(2, 'olageR', 'Para el amigo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `facturas`
+--
+
+CREATE TABLE `facturas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `venta_id` int(10) UNSIGNED NOT NULL,
+  `fecha` date NOT NULL,
+  `total` double UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(55) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `precio` double UNSIGNED NOT NULL,
+  `stock` int(10) UNSIGNED NOT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `categoria_id` int(10) UNSIGNED NOT NULL,
+  `proveedor_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `precio`, `stock`, `img`, `categoria_id`, `proveedor_id`) VALUES
+(1, 'Don satur saladas', NULL, 900, 100, 'img/donsatursaladas.jpg', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productoxventa`
+--
+
+CREATE TABLE `productoxventa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `venta_id` int(10) UNSIGNED NOT NULL,
+  `producto_id` int(10) UNSIGNED NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `subTotal` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `razonSocial` varchar(100) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`id`, `razonSocial`, `direccion`, `telefono`, `correo`) VALUES
+(1, 'Don Satur', 'calle falsa 123', '49151515', 'donsatur@hotmail.com');
 
 -- --------------------------------------------------------
 
@@ -41,15 +133,59 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nickname`, `password`, `nombre`, `apellido`, `dni`) VALUES
-(1, 'usuario1', 'password1', 'Nombre1', 'Apellido1', 12345678),
-(2, 'usuario2', 'password2', 'Nombre2', 'Apellido2', 23456789),
-(3, 'usuario3', 'password3', 'Nombre3', 'Apellido3', 34567890),
-(4, 'usuario4', 'password4', 'Nombre4', 'Apellido4', 45678901),
-(5, 'usuario5', 'password5', 'Nombre5', 'Apellido5', 56789012);
+(1, 'admin', '$2y$10$SQSmB0O9HBXcLFSe5U2s.uLSC87/.FU1x989DC.rTDfGLqnGZrXhm', 'admin', 'admin', 45878987);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `fecha` date NOT NULL,
+  `total` double UNSIGNED NOT NULL,
+  `usuario_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_facturas_ventas1_idx` (`venta_id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_productos_categorias1_idx` (`categoria_id`),
+  ADD KEY `fk_productos_proveedores1_idx` (`proveedor_id`);
+
+--
+-- Indices de la tabla `productoxventa`
+--
+ALTER TABLE `productoxventa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_ventas_has_productos_productos1_idx` (`producto_id`),
+  ADD KEY `fk_ventas_has_productos_ventas1_idx` (`venta_id`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -58,14 +194,87 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_ventas_usuarios1_idx` (`usuario_id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `productoxventa`
+--
+ALTER TABLE `productoxventa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  ADD CONSTRAINT `fk_facturas_ventas1` FOREIGN KEY (`venta_id`) REFERENCES `ventas` (`id`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `fk_productos_categorias1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`),
+  ADD CONSTRAINT `fk_productos_proveedores1` FOREIGN KEY (`proveedor_id`) REFERENCES `proveedores` (`id`);
+
+--
+-- Filtros para la tabla `productoxventa`
+--
+ALTER TABLE `productoxventa`
+  ADD CONSTRAINT `fk_ventas_has_productos_productos1` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`),
+  ADD CONSTRAINT `fk_ventas_has_productos_ventas1` FOREIGN KEY (`venta_id`) REFERENCES `ventas` (`id`);
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `fk_ventas_usuarios1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
